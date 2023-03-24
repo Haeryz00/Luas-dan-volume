935745350b2e645b32a1044e5672a32a1ef2a1cc
Haeryz00 <muh4mm4dh4r1z@gmail.com>
2023-03-24 15:50:05 +0700

    3d

--- tugasmod2/src/modul2/LKbangun3D.java
+++ tugasmod2/src/modul2/LKbangun3D.java
@@ -1,5 +1,36 @@
 package modul2;
 
+
+
 public class LKbangun3D {
+    private double volume;
+
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public void infoBangun(double a) {
+ 
+         System.out.println("Ini adalah sebuah bangun " + this.getClass().getSimpleName() + " Yang memiliki attribute : " + a);
+    }
+
+    public void infoBangun(double a, double b) {
+ 
+         System.out.println("Ini adalah sebuah bangun " + this.getClass().getSimpleName() + " Yang memiliki attribute : " + a  + " " + b);
+    }
+        public void infoBangun(double a, double b, double c) {
     
+            System.out.println("Ini adalah sebuah bangun " + this.getClass().getSimpleName() + " Yang memiliki attribute : " + a + " "+ b + " " + c);
+        }
+
+    
+
+    public void hitungVolume() {
+        System.out.println("Hasil bangun " + this.getClass().getSimpleName());
+        System.out.println("Volume: " + getVolume());
+    }
 }
--- tugasmod2/src/modul2/bangun3d/balok.java
+++ tugasmod2/src/modul2/bangun3d/balok.java
@@ -1,5 +1,65 @@
 package modul2.bangun3d;
 
-public class balok {
+import modul2.LKbangun3D;
+import java.util.Scanner;
+
+public class balok extends LKbangun3D {
+    private double panjang, lebar, tinggi;
+
+
+
     
+    
+    @Override
+    public void infoBangun(double panjang, double lebar, double tinggi) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan panjang balok: ");
+        setPanjang(input.nextDouble());
+        System.out.print("Masukkan lebar balok: ");
+        setLebar(input.nextDouble());
+        System.out.print("Masukkan tinggi balok: ");
+        setTinggi(input.nextDouble());
+    
+        input.close();
+
+        super.infoBangun(getPanjang(), getLebar(), getTinggi());
+        
+    }
+
+    @Override
+    public void hitungVolume() {
+        double volume = getPanjang() * getLebar() * getTinggi();
+        setVolume(volume);
+
+        System.out.println();
+        super.hitungVolume();
+    }
+
+    public void setPanjang(double panjang) {
+        this.panjang = panjang;
+    }
+
+    public void setLebar(double lebar) {
+        this.lebar = lebar;
+    }
+
+    public void setTinggi(double tinggi) {
+        this.tinggi = tinggi;
+    }
+
+    private double getPanjang() {
+        return panjang;
+    }
+
+    private double getLebar() {
+        return lebar;
+    }
+
+    private double getTinggi() {
+        return tinggi;
+    }
+
+    public void inputBalok() {
+    }
 }
+
